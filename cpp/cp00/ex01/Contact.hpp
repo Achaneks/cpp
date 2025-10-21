0d2c394b1f761c048ed8f5bbc2c4393d44af4a3e
Anas Chanek <achanek@c2r3p12.1337.ma>
2025-10-21 14:09:32 +0100

    push

--- cpp/cp00/ex01/Contact.hpp
+++ cpp/cp00/ex01/Contact.hpp
@@ -1,5 +1,14 @@
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 11:03:14 by achanek           #+#    #+#             */
+/*   Updated: 2025/10/21 14:07:11 by achanek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
@@ -14,23 +23,19 @@ private:
     std::string nick_name;
     std::string phone_number;
     std::string darkest_secret;
+
 public:
-    void set_first_name(std::string& s);
-    void set_last_name(std::string& s);
-    void set_nick_name(std::string& s);
-    void set_phone_number(std::string& s);
-    void set_darkest_secret(std::string& s);
+    void set_first_name(std::string &s);
+    void set_last_name(std::string &s);
+    void set_nick_name(std::string &s);
+    void set_phone_number(std::string &s);
+    void set_darkest_secret(std::string &s);
 
     std::string get_first_name();
     std::string get_last_name();
     std::string get_nick_name();
     std::string get_phone_number();
-    std::string get_darkest_secret();  
+    std::string get_darkest_secret();
 };
-
-
-
-
-
 
 #endif
--- cpp/cp00/ex01/PhoneBook.cpp
+++ cpp/cp00/ex01/PhoneBook.cpp
@@ -1,6 +1,14 @@
-
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 11:03:47 by achanek           #+#    #+#             */
+/*   Updated: 2025/10/21 14:07:33 by achanek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
@@ -9,20 +17,22 @@ void PhoneBook::newcontact(Contact &c)
     contacts[next] = c;
     if (size < 8)
         size++;
-    next = (next + 1)%8;
+    next = (next + 1) % 8;
 };
-void PhoneBook::print_all_contact()
-{
-   
+
+void PhoneBook::print_all_contact() {
+
 };
+
 void PhoneBook::print_cantact(int i)
 {
     if (i < 0 || i > 8)
         std::cout << "invalide index\n";
-    std::cout   << contacts[i].get_first_name()     << std::endl
-                << contacts[i].get_last_name()      << std::endl
-                << contacts[i].get_nick_name()      << std::endl
-                << contacts[i].get_phone_number()   << std::endl
-                << contacts[i].get_darkest_secret() << std::endl;
+    std::cout << contacts[i].get_first_name() << std::endl
+              << contacts[i].get_last_name() << std::endl
+              << contacts[i].get_nick_name() << std::endl
+              << contacts[i].get_phone_number() << std::endl
+              << contacts[i].get_darkest_secret() << std::endl;
 };
-int PhoneBook::get_size(){return size;};
+
+int PhoneBook::get_size() { return size; };

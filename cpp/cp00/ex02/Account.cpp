20a9ea134839907f73440092c06efaafffd9b2a7
Anas Chanek <achanek@c2r3p14.1337.ma>
2025-10-28 14:31:46 +0100

    push

---
+++ cpp/cp00/ex02/Account.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 13:16:05 by achanek           #+#    #+#             */
+/*   Updated: 2025/10/28 14:29:28 by achanek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+
+int Account:: _nbAccounts =  0;
+int Account:: _totalAmount = 0;
+int	Account:: _totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts( void ){return (_nbAccounts);}
+int	Account::getTotalAmount( void ){return (_totalAmount);}
+int	Account::getNbDeposits( void ){return (_totalNbDeposits);}
+int	Account::getNbWithdrawals( void ){ return (_totalNbWithdrawals);}
+
+void	Account::displayAccountsInfos( void )
+{
+    
+}
+
+Account::Account( int initial_deposit )
+{
+   _nbAccounts++;
+   _totalAmount = initial_deposit;
+   _totalNbDeposits = 1;
+   _totalNbWithdrawals = 0;
+   
+}
+Account::~Account( void )
+{
+    
+}
+
+void	Account::makeDeposit( int deposit )
+{
+    
+}
+bool	Account::makeWithdrawal( int withdrawal )
+{
+    
+}
+int		Account::checkAmount( void ) const
+{
+    
+}
+void	Account::displayStatus( void ) const
+{
+    
+}
+

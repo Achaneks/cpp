394e270038918f4f4dd9f2da0f79bf47058304ca
Anas Chanek <achanek@c3r3p14.1337.ma>
2025-11-07 10:22:14 +0100

    push

--- cpp/module02/ex02/Fixed.hpp
+++ cpp/module02/ex02/Fixed.hpp
@@ -6,14 +6,64 @@
 /*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 11:01:55 by achanek           #+#    #+#             */
-/*   Updated: 2025/11/05 11:04:54 by achanek          ###   ########.fr       */
+/*   Updated: 2025/11/05 13:46:24 by achanek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
+
 #include <iostream>
 
+class Fixed {
+private:
+    int _value;
+    static const int _bits = 8;
 
+public:
+    Fixed();
+    Fixed(const Fixed& other);
+    Fixed& operator=(const Fixed& other);
+    ~Fixed();
+
+    // constructors from int/float (from ex01)
+    Fixed(const int n);
+    Fixed(const float f);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+
+    float toFloat(void) const;
+    int toInt(void) const;
+
+    // Comparisons
+    bool operator>(const Fixed& other) const;
+    bool operator<(const Fixed& other) const;
+    bool operator>=(const Fixed& other) const;
+    bool operator<=(const Fixed& other) const;
+    bool operator==(const Fixed& other) const;
+    bool operator!=(const Fixed& other) const;
+
+    // Arithmetic
+    Fixed operator+(const Fixed& other) const;
+    Fixed operator-(const Fixed& other) const;
+    Fixed operator*(const Fixed& other) const;
+    Fixed operator/(const Fixed& other) const;
+
+    // Increment / Decrement
+    Fixed& operator++();    // pre-increment
+    Fixed operator++(int);  // post-increment
+    Fixed& operator--();    // pre-decrement
+    Fixed operator--(int);  // post-decrement
+
+    // min/max
+    static Fixed& min(Fixed& a, Fixed& b);
+    static const Fixed& min(const Fixed& a, const Fixed& b);
+    static Fixed& max(Fixed& a, Fixed& b);
+    static const Fixed& max(const Fixed& a, const Fixed& b);
+};
+
+// insertion operator (already from ex01)
+std::ostream& operator<<(std::ostream& os, const Fixed& f);
 
 #endif
--- cpp/module02/ex02/main.cpp
+++ cpp/module02/ex02/main.cpp
@@ -6,11 +6,23 @@
 /*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 11:02:09 by achanek           #+#    #+#             */
-/*   Updated: 2025/11/05 11:02:56 by achanek          ###   ########.fr       */
+/*   Updated: 2025/11/05 13:49:04 by achanek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+
+#include "Fixed.hpp"
+
+int main(void)
 {
-    
+    Fixed a;
+    Fixed const b(Fixed(5.05f) * Fixed(2));
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max(a, b) << std::endl;
+    return 0;
 }

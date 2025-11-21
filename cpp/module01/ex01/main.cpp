a95eb166e0ef650c77e6a588813ce44ec8d4de44
Anas Chanek <achanek@c2r3p14.1337.ma>
2025-11-21 20:14:40 +0100

    push

--- cpp/module01/ex01/main.cpp
+++ cpp/module01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 22:20:20 by achanek           #+#    #+#             */
-/*   Updated: 2025/11/18 16:03:32 by achanek          ###   ########.fr       */
+/*   Updated: 2025/11/18 17:08:31 by achanek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 
 int main() {
-    int N = 5;
+    int N = 10;
     Zombie* horde = zombieHorde(N, "Z_Anas");
     for (int i = 0; i < N; i++) {
         horde[i].announce();
--- cpp/module01/ex01/zombieHorde.cpp
+++ cpp/module01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 22:19:43 by achanek           #+#    #+#             */
-/*   Updated: 2025/11/18 16:01:46 by achanek          ###   ########.fr       */
+/*   Updated: 2025/11/18 17:07:57 by achanek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Zombie* zombieHorde(int N, std::string name) {
     if (N <= 0)
         return NULL;
 
-    Zombie* horde = new Zombie[N];// new[]
+    Zombie* horde = new Zombie[N];
     for (int i = 0; i < N; i++) {
         horde[i].setName(name);
     }

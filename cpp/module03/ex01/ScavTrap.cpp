acbc2bc894c37d1ce9efc38557c3dba07eea899d
Anas Chanek <achanek@c1r1p10.1337.ma>
2025-12-13 13:56:56 +0100

    after add ex02

--- cpp/module03/ex01/ScavTrap.cpp
+++ cpp/module03/ex01/ScavTrap.cpp
@@ -6,47 +6,66 @@
 /*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 09:13:19 by achanek           #+#    #+#             */
-/*   Updated: 2025/12/11 10:13:15 by achanek          ###   ########.fr       */
+/*   Updated: 2025/12/11 11:21:39 by achanek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-
-ScavTrap::ScavTrap() : ClapTrap(){
-    hitPoints = 100;
-    energyPoints = 50;
-    attackDamage = 20;
-    std::cout << "ScavTrap Default Constructor called" << std::endl;
+ScavTrap::ScavTrap() : ClapTrap("ScavTrap") {
+    this->hitPoints = 100;
+    this->energyPoints = 50;
+    this->attackDamage = 20;
+    std::cout << "ScavTrap " << this->name << " (default) constructed!" << std::endl;
 }
 
-ScavTrap::ScavTrap(std::string name): ClapTrap(name){
-    hitPoints = 100;
-    energyPoints = 50;
-    attackDamage = 20;
-    std::cout << "ScavTrap Constructor for the name " << this->name << " called" << std::endl;
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
+    this->hitPoints = 100;
+    this->energyPoints = 50;
+    this->attackDamage = 20;
+    std::cout << "ScavTrap " << this->name << " constructed!" << std::endl;
 }
 
-ScavTrap::ScavTrap(const ScavTrap& other): ClapTrap(other){
-    std::cout << "ScavTrap Copy Constructor called" << std::endl;
-}
-
-ScavTrap &ScavTrap::operator=(const ScavTrap& other){
+ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other) {
     this->name = other.name;
-    this->attackDamage = other.attackDamage;
-    this->energyPoints = other.energyPoints;
     this->hitPoints = other.hitPoints;
-    std::cout << "ScavTrap Assignation operator called" << std::endl;
+    this->energyPoints = other.energyPoints;
+    this->attackDamage = other.attackDamage;
+    std::cout << "ScavTrap " << this->name << " copied!" << std::endl;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap& other) {
+    if (this == &other)
+        return *this;
+    this->name = other.name;
+    this->hitPoints = other.hitPoints;
+    this->energyPoints = other.energyPoints;
+    this->attackDamage = other.attackDamage;
+    std::cout << "ScavTrap " << this->name << " assigned!" << std::endl;
     return *this;
 }
-ScavTrap::~ScavTrap(){
-    	std::cout << "ScavTrap Deconstructor for " << this->name << " called" << std::endl;
+
+ScavTrap::~ScavTrap() {
+    std::cout << "ScavTrap " << this->name << " destroyed!" << std::endl;
 }
 
-void ScavTrap::guardGate(){
+void ScavTrap::attack(const std::string &target) {
     
+    if (this->hitPoints <= 0) {
+        std::cout << "ScavTrap " << this->name << " is out of hit points and cannot attack." << std::endl;
+        return;
+    }
+    if (this->energyPoints <= 0) {
+        std::cout << "ScavTrap " << this->name << " has no energy and cannot attack." << std::endl;
+        return;
+    }
+    this->energyPoints--;
+    std::cout << "ScavTrap " << this->name << " viciously attacks " << target
+              << ", causing " << this->attackDamage << " points of damage! (EP left: "
+              << this->energyPoints << ")" << std::endl;
 }
 
-void ScavTrap::attack(const std::string &target){
-    
+void ScavTrap::guardGate() {
+    std::cout << "ScavTrap " << this->name << " is now in Gate keeper mode." << std::endl;
 }
+
--- cpp/module03/ex01/main.cpp
+++ cpp/module03/ex01/main.cpp
@@ -6,7 +6,37 @@
 /*   By: achanek <achanek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 09:13:13 by achanek           #+#    #+#             */
-/*   Updated: 2025/12/11 09:13:14 by achanek          ###   ########.fr       */
+/*   Updated: 2025/12/11 11:25:57 by achanek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScavTrap.hpp"
+
+int main() {
+    std::cout << "=== Creating ScavTrap a ===" << std::endl;
+    ScavTrap a("Serena");
+
+    std::cout << "\n=== a attacks target1 ===" << std::endl;
+    a.attack("target1");
+
+    std::cout << "\n=== a enters guard mode ===" << std::endl;
+    a.guardGate();
+
+    std::cout << "\n=== Copy-constructing b from a ===" << std::endl;
+    ScavTrap b(a);
+
+    std::cout << "\n=== b attacks target2 ===" << std::endl;
+    b.attack("target2");
+
+    std::cout << "\n=== Creating c (default) ===" << std::endl;
+    ScavTrap c;
+
+    std::cout << "\n=== Assigning c = a ===" << std::endl;
+    c = a;
+
+    std::cout << "\n=== c attacks target3 ===" << std::endl;
+    c.attack("target3");
+
+    std::cout << "\n=== End of main, destructors will run in reverse order ===" << std::endl;
+    return 0;
+}
